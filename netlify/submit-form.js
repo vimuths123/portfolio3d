35168b68f5ed430a66ef2eac173a0c3507ebd889
vimuths123 <vimuths@yahoo.com>
2023-04-11 12:25:00 +0530

    Add function

---
+++ netlify/submit-form.js
@@ -0,0 +1,8 @@
+exports.handler = async (event) => {
+    const { name, email, message } = JSON.parse(event.body);
+    // Do something with the form data, such as send it to a database or send an email
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'Form submitted successfully!' }),
+    };
+};
