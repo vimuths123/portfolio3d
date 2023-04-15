dae2c6dc432bc05f79713c4fe1b3072ffc21603d
vimuths123 <vimuths@yahoo.com>
2023-04-15 22:52:23 +0530

    get token

--- netlify/submit-form.js
+++ netlify/submit-form.js
@@ -1,9 +1,53 @@
-exports.handler = async (event) => {
-    const { name, email, message } = JSON.parse(event.body);
+// exports.handler = async (event) => {
+//     const { name, email, message } = JSON.parse(event.body);
     
-    // Do something with the form data, such as send it to a database or send an email
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: 'Form submitted successfully!' }),
-    };
+//     // Do something with the form data, such as send it to a database or send an email
+//     return {
+//         statusCode: 200,
+//         body: JSON.stringify({ message: 'Form submitted successfully!' }),
+//     };
+// };
+
+const {google} = require('googleapis');
+const nodemailer = require('nodemailer');
+const OAuth2 = google.auth.OAuth2;
+
+exports.handler = async (event, context) => {
+  const oAuth2Client = new OAuth2(
+    process.env.GMAIL_CLIENT_ID,
+    process.env.GMAIL_CLIENT_SECRET,
+    "https://developers.google.com/oauthplayground"
+  );
+
+  oAuth2Client.setCredentials({
+    refresh_token: process.env.GMAIL_REFRESH_TOKEN
+  });
+
+  const accessToken = await oAuth2Client.getAccessToken();
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: process.env.EMAIL_ADDRESS,
+      clientId: process.env.GMAIL_CLIENT_ID,
+      clientSecret: process.env.GMAIL_CLIENT_SECRET,
+      refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+      accessToken: accessToken
+    }
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_ADDRESS,
+    to: 'recipient@example.com',
+    subject: 'Test email',
+    text: 'This is a test email'
+  };
+
+  const result = await transporter.sendMail(mailOptions);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({message: 'Email sent successfully'})
+  };
 };
